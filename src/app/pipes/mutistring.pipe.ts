88e170ff2ea221ab5ae113b9f5e6255d64b3a934
Arjun Krishnamoorthy <krisaju95@gmail.com>
2019-05-12 23:28:04 +0530

    Osdfdsf

--- src/app/pipes/mutistring.pipe.ts
+++ src/app/pipes/mutistring.pipe.ts
@@ -7,9 +7,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MultiStringPipe implements PipeTransform {
 	transform(inputString: string, maxLength: number): string {
 		let formattedString: string = '';
-		let stringArray: Array<string>= (inputString || '').split(' ');
+		let stringArray: Array<string> = (inputString || '').split(' ');
 		let stringElementArray: Array<string> = [];
-		for(let stringIndex = 0; stringIndex < stringArray.length; stringIndex++) {
+		for (let stringIndex = 0; stringIndex < stringArray.length; stringIndex++) {
 			let subStringArray: Array<string> = stringArray.slice(stringIndex, stringIndex + maxLength);
 			subStringArray = this.formatSubstring(subStringArray)
 			let stringElement: string = '<span>' + subStringArray.join(' ') + '</span>';
@@ -21,8 +21,10 @@ export class MultiStringPipe implements PipeTransform {
 	}
 
 	formatSubstring(subStringArray: Array<string>): Array<string> {
-		if((subStringArray || [])[0] == '-') {
+		if ((subStringArray || [])[0] == '-') {
 			subStringArray.shift();
+		} else if ((subStringArray || [])[subStringArray.length - 1] == '-') {
+			subStringArray.pop();
 		}
 		return subStringArray;
 	}
