9110a7899d192d568113df90bbea1300ff06e4ee
Arjun Krishnamoorthy <krisaju95@gmail.com>
2020-01-05 17:06:35 -0500

    Deleted App

--- src/app/site-body/fifa/official-soundtracks/data/soundtracks/fifa-15.soundtrack.ts
+++
@@ -1,6 +0,0 @@
-export const FIFA15SoundTrack: object = {
-    title: "FIFA 15 Soundtrack",
-    thumbURL: 'https://pl.scdn.co/images/pl/default/be4b279ef8a21bddfbea90bbd9f185c75eb3e67f',
-    year: "2014",
-    tracks: []
-}
--- src/app/site-body/fifa/official-soundtracks/data/soundtracks/fifa-17.soundtrack.ts
+++
@@ -1,6 +0,0 @@
-export const FIFA17SoundTrack: object = {
-    title: "FIFA 17 Soundtrack",
-    thumbURL: 'https://pl.scdn.co/images/pl/default/7139cf17fe158e913710bf327750e8f34a56d4bc',
-    year: "2016",
-    tracks: []
-}
