8144e826109e44bf02a61264d0ce753528790264
Arjun Krishnamoorthy <krisaju95@gmail.com>
2020-01-08 20:53:19 -0500

    Fixed Console error

--- src/app/pipes/profile-info/profile-info.pipe.ts
+++ src/app/pipes/profile-info/profile-info.pipe.ts
@@ -7,16 +7,21 @@ import { profiles } from '../../constants/wa-profiles';
 
 export class WAProfileInfoPipe implements PipeTransform {
 	transform(profileID: string, infoType: string = 'handle'): any {
-		switch (infoType) {
-			case 'handle': {
-				return profiles[profileID].handle;
-			}
-			case 'twitter': {
-				return profiles[profileID].twitter;
-			}
-			case 'website': {
-				return profiles[profileID].website;
+		const profile: any = profiles[profileID];
+		let data: any = '';
+		if (profile) {
+			switch (infoType) {
+				case 'handle': {
+					data = profiles[profileID].handle; break;
+				}
+				case 'twitter': {
+					data = profiles[profileID].twitter; break;
+				}
+				case 'website': {
+					data = profiles[profileID].website; break;
+				}
 			}
 		}
+		return data || '';
 	}
 }
