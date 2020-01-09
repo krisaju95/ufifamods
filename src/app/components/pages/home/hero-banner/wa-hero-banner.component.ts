89538b05b1a4577bbd6a26a7f6a1e8e1e99ed02e
Arjun Krishnamoorthy <krisaju95@gmail.com>
2020-01-08 20:29:34 -0500

    Some tings

--- src/app/components/pages/home/hero-banner/wa-hero-banner.component.ts
+++ src/app/components/pages/home/hero-banner/wa-hero-banner.component.ts
@@ -17,6 +17,8 @@ export class WAHeroBannerComponent {
 
 	heroBannerPost: any = {};
 
+	description: string = '';
+
 	constructor(
 		private WADBService: WADBService,
 		private WALoaderService: WALoaderService
@@ -28,6 +30,7 @@ export class WAHeroBannerComponent {
 				this.blogPostList = this.WADBService.getBlogPostsList();
 				this.filteredBlogPosts = this.WADBService.filterPostsData(this.blogPostList, 'featured', 1);
 				this.heroBannerPost = this.filteredBlogPosts[0];
+				this.description = ((this.heroBannerPost['post-description'] || this.heroBannerPost['post-text-content'] || '') as string).slice(0, 100).trim();
 				const matches: RegExpMatchArray = (this.heroBannerPost['post-title'] as string).match(new RegExp(/^FIFA\s[0-9]+\:/)) || [];
 				if (matches[0]) {
 					this.heroBannerPost['post-title'] = this.heroBannerPost['post-title'].replace(matches[0], '<span class="wa-title-highlight">' + matches[0] + '</span>');
