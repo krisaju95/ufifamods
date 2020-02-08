906f5136dd99110e2bb7d03fe8e99daf0bc02b73
Arjun Krishnamoorthy <krisaju95@gmail.com>
2020-02-08 14:16:49 -0500

    New Tings

--- src/app/components/common/post-card/wa-post-card.component.ts
+++ src/app/components/common/post-card/wa-post-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { WABlogPost } from 'src/app/interfaces/blog-post.interface';
 
 @Component({
 	selector: 'ufm-wa-post-card',
@@ -7,7 +8,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class WAPostCardComponent {
 	
-	@Input() post: any;
+	@Input() post: WABlogPost;
 
 	@Input() loading: boolean;
 
