f9c6c6b71b8de72788bfdc78d86e391d9febf047
Arjun Krishnamoorthy <krisaju95@gmail.com>
2021-09-02 14:15:34 -0400

    Trying focus blur thingy

--- src/app/components/pages/create-fut-card/create-fut-card.component.ts
+++ src/app/components/pages/create-fut-card/create-fut-card.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class CreateFUTCardComponent {
     phoneNumber: string = "2314512312";
+
+    onFocus(event: Event): void {
+        event.preventDefault();
+    }
+
+    onFocus2(event: Event): void {
+        this.onFocus(event);
+        event.stopPropagation();
+    }
+
+    onFocus3(event: Event): void {
+        this.onFocus2(event);
+        event.stopImmediatePropagation();
+    }
+
+    onFocus4(event: Event): void {
+        event.preventDefault();
+        window.blur();
+    }
+
+    onFocus5(event: Event): void {
+        event.preventDefault();
+        setTimeout(() => {
+            window.blur();
+        }, 50)
+    }
 }
